d87cded46e4d2eb5f0a7c77b36fd8dbd030c6539
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-16 14:44:26 +0100

    Move .places styles to module

--- components/venues.js
+++ components/venues.js
@@ -1,8 +1,9 @@
 import Card from '../components/Card';
+import styles from './venues.module.css';
 
 export default function Venues(props) {
   return (
-    <div className={'places'}>
+    <div className={styles.places}>
       <h2>Places</h2>
       <ul>
         {
