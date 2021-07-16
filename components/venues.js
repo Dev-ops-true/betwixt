1f50dcae7a23a48465f43ea38cf53a1960971d59
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-16 14:45:22 +0100

    Destructure props to places

--- components/venues.js
+++ components/venues.js
@@ -1,13 +1,13 @@
 import Card from '../components/Card';
 import styles from './venues.module.css';
 
-export default function Venues(props) {
+export default function Venues({places}) {
   return (
     <div className={styles.places}>
       <h2>Places</h2>
       <ul>
         {
-          props.places.map((place) => (
+          places.map((place) => (
             <Card key={place.name} name={place.name} rating={place.rating}></Card>
           ))
         }
