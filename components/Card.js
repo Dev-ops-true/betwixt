4fe1bcd95054ef0059948e8f17e47ef3ed410224
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-16 16:55:44 +0100

    Add star icons to show rating

--- components/Card.js
+++ components/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactStars from "react-rating-stars-component";
 import styles from './Card.module.css'
 
 export default function Card(props) {
@@ -9,6 +10,13 @@ export default function Card(props) {
       <span>{props.rating}</span>
       <span>{`(${props.ratings_num})`}</span>
     </p>
+    <ReactStars
+      size={20}
+      max={5}
+      value={props.rating}
+      isHalf={true}
+      edit={false}
+    />
     <p>{props.address}</p>
     <img className={styles.card_image} src={props.photo_link}></img>
   </div>
