f0282b6c51958f03a6f9583d7243731dcc5ab6ed
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-16 16:18:25 +0100

    Add stars to the card

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '@fortawesome/fontawesome-free/css/all.css';
 
 export default function Betwixt({ Component, pageProps }) {
   return <Component {...pageProps} />
