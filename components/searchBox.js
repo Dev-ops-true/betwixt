087e1f6eaefcb2f00a1532512aa2df4df65e8526
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-17 16:32:25 +0100

    add an input box for selecting category of venue

--- components/searchBox.js
+++ components/searchBox.js
@@ -1,13 +1,18 @@
-export default function SearchBox({handleSubmit}) {
+export default function SearchBox({ handleSubmit }) {
   return (
     <form className="search" onSubmit={(e) => handleSubmit(e)}>
-      <input type="text" name="origin" placeholder="Your post code"/>
-      <input type="text" name="destination" placeholder="Your friend's post code"/>
+      <input type="text" name="origin" placeholder="Your post code" />
+      <input type="text" name="destination" placeholder="Your friend's post code" />
       <select name="travelMode">
         <option value="DRIVING">Driving</option>
         <option value="WALKING">Walking</option>
         <option value="BICYCLING">Bicycling</option>
         <option value="TRANSIT">Transit</option>
+      </select>
+      <select name="category">
+        <option value="RESTAURANT">Restaurant</option>
+        <option value="BAR">Bar</option>
+        <option value="CAFE">Cafe</option>
       </select>
       <button type="submit">Search</button>
     </form>
