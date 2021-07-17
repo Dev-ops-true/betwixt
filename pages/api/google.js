9793c4a1feccd05e848211827cd3d3e29f52c02d
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-17 19:00:32 +0100

    Combine 3 APIs with 3 different venue types with Promise.all

--- pages/api/google.js
+++ pages/api/google.js
@@ -4,7 +4,14 @@ export default async function handler(req, res) {
   const lat = req.body.lat
   const lng = req.body.lng
 
-  const response = await nodeFetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBXlpinTY2iWYVXDuFFbE9PnrPIr7cfNHk`)
-  const jsonResponse = await response.json()
+  const response = await Promise.all([
+    nodeFetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1500&type=restaurant&key=AIzaSyBXlpinTY2iWYVXDuFFbE9PnrPIr7cfNHk`),
+    nodeFetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1500&type=bar&key=AIzaSyBXlpinTY2iWYVXDuFFbE9PnrPIr7cfNHk`),
+    nodeFetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1500&type=cafe&key=AIzaSyBXlpinTY2iWYVXDuFFbE9PnrPIr7cfNHk`)
+  ]);
+
+  const jsonResponse = await Promise.all(response.map(r => r.json()))
+
   res.status(200).json(jsonResponse)
+  console.log(jsonResponse)
 }
