1ba1fedb4ecca979e8cd1e3829ca1a2cbfde3d88
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-17 12:22:59 +0100

    Set imr src to places photos api

---
+++ components/card.js
@@ -0,0 +1,1 @@
+    <img className={styles.card_image} src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=200&photoreference=${photo_reference}&key=${process.env.NEXT_PUBLIC_API_KEY}`} />
