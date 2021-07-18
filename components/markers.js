3c5bc07406dd8de7efaa6bd5ef31b123bdcc7f4b
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-18 18:46:52 +0100

    add initial InfoWindow to Markers

--- components/markers.js
+++ components/markers.js
@@ -1,12 +1,37 @@
-import { Marker } from '@react-google-maps/api';
+import { Marker, InfoWindow } from '@react-google-maps/api';
+import { useState } from 'react';
 
 export default function Markers({ places }) {
+  const [open, setOpen] = useState(false);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+
+  const mouseOverHandler = (place) => {
+    setOpen(true)
+    setSelectedPlace(place)
+  }
+
+  const mouseOutHandler = () => {
+    setOpen(false)
+    setSelectedPlace(null)
+  }
+
+
   return (
     <>
       {
         places.map(place => {
           return (
-            <Marker key={place.place_id} position={place.geometry.location} title={place.name} />
+            <Marker key={place.place_id} position={place.geometry.location} title={place.name} onMouseOver={() => mouseOverHandler(place)} onMouseOut={() => mouseOutHandler}>
+              {selectedPlace && (
+                <InfoWindow
+                  position={selectedPlace.vicinity}
+                  onCloseClick={() => mouseOutHandler}
+                >
+                  <span>{selectedPlace.name}</span>
+                </InfoWindow>
+              )
+              }
+            </Marker>
           )
         })
       }
