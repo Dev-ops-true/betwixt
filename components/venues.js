58175525b310cac2b76d102b32ae4bafd6a33678
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-18 10:44:45 +0100

    handle edge cases - when no places or no photos

--- components/venues.js
+++ components/venues.js
@@ -7,13 +7,13 @@ export default function Venues({ places }) {
       <h2>Places</h2>
       <ul>
         {
-          places.map((place) => (
+          places && places.map((place) => (
             <Card key={place.name}
               name={place.name}
               rating={place.rating}
               address={place.vicinity}
               ratings_num={place.user_ratings_total}
-              photo_reference={place.photos[0].photo_reference}>
+              photo_reference={place.photos && place.photos[0].photo_reference}>
             </Card>
           ))
         }
