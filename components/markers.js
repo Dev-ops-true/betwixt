a84ed2dcb839d694f375ba966251fefaff7ae5c2
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-18 15:14:30 +0100

    extract lat/long from the api response

--- components/markers.js
+++ components/markers.js
@@ -3,7 +3,7 @@ import { Marker } from "@react-google-maps/api";
 export default function Markers({ places }) {
   return (
     places.map(place => {
-      <Marker key={place.place_id} position={place.geometry} title={place.name} />
+      <Marker key={place.place_id} position={place.geometry.location} title={place.name} />
     })
   )
 };
