abb41b06846594f14619bae8a7bf9828e024122c
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-19 12:16:16 +0100

    add InfoWindow place details on Marker hover

--- components/markers.js
+++ components/markers.js
@@ -1,17 +1,58 @@
-import { Marker } from '@react-google-maps/api';
+import { Marker, InfoWindow } from '@react-google-maps/api';
+import { useState } from 'react';
+import ReactStars from "react-rating-stars-component";
 
 export default function Markers({ places }) {
+  const [open, setOpen] = useState(false);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+
+  const mouseOverHandler = (place) => {
+    setOpen(true)
+    setSelectedPlace(place)
+  }
+
+  const mouseOutHandler = () => {
+    setOpen(false)
+    setSelectedPlace(null)
+  }
+
   return (
     <>
       {
         places.map(place => {
           return (
-            <Marker key={place.place_id} position={place.geometry.location} title={place.name} />
+            <Marker key={place.place_id} position={place.geometry.location} title={place.name} onMouseOver={() => mouseOverHandler(place)} onMouseOut={() => mouseOutHandler}>
+              {open && selectedPlace && selectedPlace.name === place.name && (
+                <InfoWindow
+                  position={selectedPlace.vicinity}
+                  onCloseClick={() => mouseOutHandler}
+                >
+                  <div>
+                    <p>{selectedPlace.name}</p>
+                    <p>
+                      {selectedPlace.rating}
+                      <ReactStars
+                        size={20}
+                        max={5}
+                        value={selectedPlace.rating}
+                        isHalf={true}
+                        edit={false}
+                      />
+                      {`(${selectedPlace.user_ratings_total})`}
+                    </p>
+                    <p>{selectedPlace.vicinity}</p>
+                  </div>
+                </InfoWindow>
+              )
+              }
+            </Marker>
           )
         })
       }
     </>
   )
 };
+
+
 
 
