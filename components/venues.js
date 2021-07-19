6e9f5792622c172a1d08f414f7862e285c7a6e93
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-19 19:38:30 +0100

    implement Modal functionality inside Venues

--- components/venues.js
+++ components/venues.js
@@ -1,7 +1,49 @@
+import { useState } from 'react';
 import Card from './card';
 import styles from './venues.module.css';
+import Modal from 'react-modal';
+import { Autocomplete } from '@react-google-maps/api';
+
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#__next');
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    paddingLeft: '-50%',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: 10000
+  },
+};
 
 export default function Venues({ places }) {
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [activeCard, setActiveCard] = useState(null);
+
+  const openModal = (place) => {
+    setIsOpen(true);
+    setActiveCard(place)
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className={styles.places}>
       <h2>Your places.</h2>
@@ -24,6 +66,24 @@ export default function Venues({ places }) {
           ))
         }
       </ul>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal">
+        {
+          activeCard && <Card key={activeCard.name}
+            name={activeCard.name}
+            rating={activeCard.rating}
+            address={activeCard.vicinity}
+            ratings_num={activeCard.user_ratings_total}
+            photo_reference={activeCard.photos && activeCard.photos[0].photo_reference}
+            onClick={() => openModal(activeCard)}>
+          </Card>
+        }
+        <button className="button" onClick={closeModal}>close</button>
+      </Modal>
     </div>
   )
 }
