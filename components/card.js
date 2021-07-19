bd297b457d587e69c1e498591b71b9e92b95c2e5
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-19 10:23:05 +0100

    Add back card.js

---
+++ components/card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactStars from "react-rating-stars-component";
+import styles from './Card.module.css'
+import Image from 'next/image'
+
+export default function Card({ name, photo_reference, rating, ratings_num, address }) {
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.card_info}>
+        <p className={styles.card_title}>{name}</p>
+        <p>
+          {rating}
+          <ReactStars
+            size={20}
+            max={5}
+            value={rating}
+            isHalf={true}
+            edit={false}
+          />
+          <span>{`(${ratings_num})`}</span>
+        </p>
+        <p>{address}</p>
+      </div>
+      <div>
+        <Image className={styles.card_photo} width={100} height={100} alt='Venue image' src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=100&photoreference=${photo_reference}&key=${process.env.NEXT_PUBLIC_API_KEY}`} />
+      </div>
+    </div>
+  )
+}
