d63f51c13a95bf455591def7470236f0c984ce67
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-20 13:45:37 +0100

    create DetailedCard component

---
+++ components/detailedCard.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import React from 'react';
+import ReactStars from "react-rating-stars-component";
+import styles from './detailedCard.module.css'
+import Image from 'next/image'
+export default function DetailedCard({ place_id, photo_reference }) {
+
+  return (
+    <>
+    </>
+  )
+}
