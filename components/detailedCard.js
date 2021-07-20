140c1a6b5aec6b8d788665c602110b97b7298e6e
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-20 13:46:32 +0100

    Fetch  Details API data using useEffect hook

--- components/detailedCard.js
+++ components/detailedCard.js
@@ -5,6 +5,25 @@ import styles from './detailedCard.module.css'
 import Image from 'next/image'
 export default function DetailedCard({ place_id, photo_reference }) {
 
+  const [activePlace, setActivePlace] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const activePlace = await fetch(
+        '/api/google_place_details', {
+        method: 'POST',
+        body: JSON.stringify({ place_id: place_id }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      const activePlaceJson = await activePlace.json()
+      setActivePlace(activePlaceJson.result)
+      console.log(activePlaceJson.result)
+    }
+    fetchData();
+  }, [place_id]);
   return (
     <>
     </>
